6e200e2789c2ce027daf86880d3b9314a1d33149
Іра <iryna.nazar@ukma.edu.ua>
2023-06-03 23:49:11 -0400

    Map component, Country autofill,Counter components,price and image adding + route

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,9 +8,16 @@ import {useMemo, useState} from "react";
 import {Heading} from "@/app/components/Heading";
 import {categories} from "@/app/components/navbar/Categories";
 import CategoryInput from "@/app/components/inputs/CategoryInput";
-import {FieldValues, useForm} from "react-hook-form";
+import {FieldValues, SubmitHandler, useForm} from "react-hook-form";
 import CountrySelect from "@/app/components/inputs/CountrySelect";
 import Map from "@/app/components/Map";
+import dynamic from "next/dynamic";
+import Counter from "@/app/components/inputs/Counter";
+import ImageUpload from "@/app/components/inputs/ImageUpload";
+import Input from "@/app/components/inputs/Input";
+import axios from "axios";
+import toast from "react-hot-toast";
+import {useRouter} from "next/navigation";
 
 
 enum STEPS {
@@ -23,10 +30,10 @@ enum STEPS {
 }
 
 const RentModal = () => {
-
+    const router = useRouter()
     const rentModal = useRentModal();
 
-
+    const [isLoading, setIsLoading] = useState(false);
     const [step, setStep] = useState(STEPS.CATEGORY);
 
     const {
@@ -60,6 +67,10 @@ const RentModal = () => {
     const imageSrc = watch('imageSrc');
 
 
+    const Map = useMemo(() => dynamic(() => import('../Map'), {
+        ssr: false
+    }), [location]);
+
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
             shouldDirty: true,
@@ -75,6 +86,30 @@ const RentModal = () => {
     const onNext = () => {
         setStep((value) => value + 1);
     }
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        if (step !== STEPS.PRICE) {
+            return onNext();
+        }
+
+        setIsLoading(true);
+
+        axios.post('/api/listings', data)
+            .then(() => {
+                toast.success('Listing created!');
+                router.refresh();
+                reset();
+                setStep(STEPS.CATEGORY)
+                rentModal.onClose();
+            })
+            .catch(() => {
+                toast.error('Something went wrong.');
+            })
+            .finally(() => {
+                setIsLoading(false);
+            })
+    }
+
 
     const actionLabel = useMemo(() => {
         if (step === STEPS.PRICE) {
